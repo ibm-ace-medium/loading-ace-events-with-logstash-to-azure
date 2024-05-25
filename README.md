a6b78d6c5bbed07cf5ca13b7a090e2aab1cf276b
Ivan Slavka <islavka2@gmail.com>
2024-05-25 16:09:01 +0200

    Initial implementation.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # loading-ace-events-with-logstash-to-azure
+
+# follow logs from logstash-server
+sudo docker compose logs logstash-server -f
+# enter integration-server container bash
+sudo docker compose exec integration-server bash
---
+++ update.sh
@@ -0,0 +1,7 @@
+. ~/Tools/ace-12.0.8.0/server/bin/mqsiprofile
+
+ibmint package --input-path ~/Repositories/ibm-ace-medium/loading-ace-events-with-logstash-to-azure/LoadingAceEventsWithLogstashToAzure \
+--output-bar-file ~/Repositories/ibm-ace-medium/loading-ace-events-with-logstash-to-azure/integration-server/LoadingAceEventsWithLogstashToAzure.bar \
+--do-not-compile-java
+
+sudo docker compose up -d --build --remove-orphans
